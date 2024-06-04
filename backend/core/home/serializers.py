b0d7baf2e70239f86993d83b12de8e43695a056e
Riya Agrawal <riyaag0105@gmail.com>
2024-06-04 16:44:05 +0530

    updated backend code

--- backend/core/home/serializers.py
+++ backend/core/home/serializers.py
@@ -6,18 +6,21 @@ class StudentSerializer(serializers.Serializer):
         model = Student
         fields='__all__'
 
-    def create(self,validated_data):
-        return Student.objects.create(**validated_data) 
+        # def create(self,validated_data):
+        #     return Student.objects.create(**validated_data) 
 
-    def read(self,validated_data):
-        return Student.objects.all()
+        # def read(self):
+        #     return Student.objects.all()
 
-    def update(self,instance,validated_data):
-        instance.rollno = validated_data.get('rollno', instance.rollno)
-        instance.name= validated_data.get('name', instance.name)
-        instance.department = validated_data.get('department', instance.department)
-        instance.subject= validated_data.get('subject', instance.subject)   
-        instance.remarks= validated_data.get('remarks', instance.remarks)   
-        instance.application_document= validated_data.get('application_document', instance.application_document)
-        instance.state= validated_data.get('state', instance.state)
+        # def update(self,instance,validated_data):
+        #     instance.rollno = validated_data.get('rollno', instance.rollno)
+        #     instance.name= validated_data.get('name', instance.name)
+        #     instance.department = validated_data.get('department', instance.department)
+        #     instance.subject= validated_data.get('subject', instance.subject)   
+        #     instance.remarks= validated_data.get('remarks', instance.remarks)   
+        #     instance.application_document= validated_data.get('application_document', instance.application_document)
+        #     instance.state= validated_data.get('state', instance.state)
+        #     instance.save()
+        #     return instance
         
+
--- backend/core/home/views.py
+++ backend/core/home/views.py
@@ -1,13 +1,18 @@
+#import logging
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import StudentSerializer  # Add the missing import statement
 # Create your views here.
+
+#logger=logging.getLogger('home')
+
 @api_view(['GET'])
 def home(request):
     student_objs = Student.objects.all()
     serializer = StudentSerializer(student_objs,many=True) 
+    #logger.debug(f'Number of students retrieived: {student_objs.count()}')
     return Response({'data': serializer.data})
 
 
@@ -22,36 +27,43 @@ def compose(request):
 
 @api_view(['GET'])
 def outward(request):
-    student_objs = Student.objects.filter(state='outward')
+    student_objs = Student.objects.filter(state=1)
     serializer = StudentSerializer(student_objs,many=True) 
     return Response({'data': serializer.data})
 
 
 @api_view(['GET'])
 def inward(request):
-    student_objs = Student.objects.filter(state='inward')
+    student_objs = Student.objects.filter(state=0)
     serializer = StudentSerializer(student_objs,many=True)  
     return Response({'data': serializer.data})
 
 
-@api_view(['PUT'])
+@api_view(['PATCH'])
 def update_inward(request):
-    data = request.data
-    instance = Student.objects.get(id=data.get('id'))  # Define the "instance" variable
-    instance.roll_number = data.get('roll_number')
-    instance.name = data.get('name')
-    instance.department = data.get('department')
-    instance.subject = data.get('subject')
-    instance.remarks = data.get('remarks')
-    instance.application_document = data.get('application_document')
-    instance.state = data.get('state')
-    student_objs = Student.objects.filter(id=data.get('id'))
-    serializer = StudentSerializer(student_objs,many=True)  
-    return Response({'data': serializer.data})
+    try:
+        data = request.data
+        instance = Student.objects.get(id=data.get('id'))  # Define the "instance" variable
+        serializer = StudentSerializer(instance, data, partial=True) 
+        #    instance.roll_number = data.get('roll_number')
+        #  instance.department = data.get('department')
+        #   instance.subject = data.get('subject')
+        #   instance.remarks = data.get('remarks')
+        #   instance.application_document = data.get('application_document')
+        #   instance.state = data.get('state')
+        #student_objs = Student.objects.filter(id=data.get('id'))
+        if not serializer.is_valid:
+                print(serializer.errors)
+                return Response({'status': 403, 'errors': serializer.errors, 'message': 'Something went wrong'})
+        serializer.save()
+        return Response({'status' : 200, 'data': serializer.data, 'message': 'Your data has been saved'})
+    except Exception as e:
+        print(e)
+        return Response({'status': 403, 'errors': serializer.errors, 'message': 'An error occcured'})
 
 ##fetch using id
 @api_view(['GET'])
-def fetch(request,id):
+def fetch(request, id):
     student_objs = Student.objects.get(id=id)
     serializer = StudentSerializer(student_objs)  
     return Response({'data': serializer.data})
